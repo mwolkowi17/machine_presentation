91bc94ce6684b9ab99fe0725dc89c43176a2f68f
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2023-04-29 13:09:23 +0200

    cleaning

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,8 @@
 
 import React from "react";
 
-export const Button1=(props)=>{
-    return(
+export const Button1 = (props) => {
+    return (
         <button type="button" className="btn btn-primary btn-lg" id="przycisk" onClick={props.click}>
             Start/Stop
         </button>
--- src/Loader2.jsx
+++ src/Loader2.jsx
@@ -1,6 +1,6 @@
 import { useLoader, useFrame } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import React, { useRef,Suspense,useMemo } from 'react';
+import React, { useRef, Suspense, useMemo } from 'react';
 
 
 export const Bearing = (props) => {
